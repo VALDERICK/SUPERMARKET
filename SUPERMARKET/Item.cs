09e42d5e0cba0653b6390af29128e98844fbdd94
Yary <a2308746@institutmontilivi.cat>
2024-04-30 22:24:59 +0200

    Arreglo calculo años, Inicio  class Item

--- SUPERMARKET/Item.cs
+++ SUPERMARKET/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,27 @@ namespace SUPERMARKET
 {
     internal class Item : IComparable<Item>
     {
-        #region ATRIBUTS
 
+        #region ENUMS
+        public enum Category
+        { BERVERAGE = 1, FRUITS, VEGETABLES, BREAD, MILK_AND_DERIVATES, GARDEN, MEAT, SWEETS, SAUCES, FROZEN, CLEANING, FISH, OTHER };
+
+        public enum Packaging
+        {Unit, Kg, Package};
         #endregion
+
+        #region ATRIBUTS
+        char currency = '\u20AC';
+        int code;
+        string description;
+        bool onSale;
+        double price;
+        Category category;
+        Packaging packaging;
+        double stock;
+        int minStock;
+        #endregion
+
         public int CompareTo(Item? other)
         {
             throw new NotImplementedException();
