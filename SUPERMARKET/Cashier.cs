acdd2607e922def90b60962279bfce4916bcb949
Yary <a2308746@institutmontilivi.cat>
2024-04-30 18:23:07 +0200

    cashier

---
+++ SUPERMARKET/Cashier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUPERMARKET
+{
+    internal class Cashier
+    {
+        #region ATRIBUTES
+        private DateTime _joibingDate;
+        public int YearsOfService;
+        public int Points=0;
+        #endregion
+
+        #region METHODS
+        public void AddPoints(int pointsToAdd)
+        {
+            
+        }
+        #endregion
+
+        #region PROPERTY
+        public override double GetRating()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
