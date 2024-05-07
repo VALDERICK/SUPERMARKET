ce8d8a05f21505e5c2cbbdaef4f0331e809eb04a
Yary <a2308746@institutmontilivi.cat>
2024-05-07 11:27:16 +0200

    si

--- SUPERMARKET/Supermarket.cs
+++ SUPERMARKET/Supermarket.cs
@@ -1,113 +1,123 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace SUPERMARKET
-//{
-//    internal class Supermarket
-//    {
-//        #region ATRIBUTS
-//        private string name;
-//        private string address;
-//        public static int MAXLINES = 5;
-//        private int activeLines;
-//        private CheckOutLine[] lines = new CheckOutLine[MAXLINES];
-//        private Dictionary<string, Person> staff;
-//        private Dictionary<string, Person> customers;
-//        private SortedDictionary<int, Item> warehouse;
+namespace SUPERMARKET
+{
+    internal class Supermarket
+    {
+        #region ATRIBUTS
+        private string name;
+        private string address;
+        public static int MAXLINES = 5;
+        private int activeLines;
+        private CheckOutLine[] lines = new CheckOutLine[MAXLINES];
+        private Dictionary<string, Person> staff;
+        private Dictionary<string, Person> customers;
+        private SortedDictionary<int, Item> warehouse;
 
-//        #endregion
+        public class CheckOutLine
+        {
+            private int number;
+            //private Queue<ShoppingCart> queue;
+            private Person cashier;
+            private bool active;
+        }
 
-//        #region METHODS
-//        private Dictionary<string, Person> LoadCustomers(string fileName)
-//        {
-//            StreamReader sr = new StreamReader(fileName);
-//            string linia;
+        public Supermarket(string name, string address, string fileCustomers, string fileItems, int activeLines) : this(name, address)
+        {
+            LoadCustomers("CUSTOMERS.TXT");
+            LoadCashiers("CASHIERS.TXT");
+            LoadWarehouse("GROCERIES.TXT");
+            this.activeLines = activeLines;
+        }
 
-//            linia = sr.ReadLine();
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
 
-//            while (linia != null)
-//            {
-//                string[] infoClient = linia.Split(';');
-//                string id = infoClient[0];
-//                string nomComplet = infoClient[1].Trim();
-//                string tarjetaFidelitat;
-//                if (infoClient.Length > 2)
-//                {
-//                    tarjetaFidelitat = infoClient[2];
-//                }
-//                else
-//                {
-//                    tarjetaFidelitat = "";
-//                }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
 
-//                Person customer = new Person(id, nomComplet, tarjetaFidelitat);
-//                customers[id] = customer;
-//            }
+        public int ActiveLines
+        {
+            get { return activeLines; }
+            set
+            {
+                if (value >= 1 && value <= MAXLINES)
+                {
+                    activeLines = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Active lines must be between 1 and MAXLINES.");
+                }
+            }
+        }
 
-//        }
-//        private Dictionary<string, Person> LoadCashiers(string fileName)
-//        {
+        public void InitializeCheckOutLines()
+        {
+            for (int i = 0; i < MAXLINES; i++)
+            {
+                lines[i] = new CheckOutLine(); // Initialize each checkout line
+            }
+        }
 
-//        }
-//        private Dictionary<string, Person> LoadWarehouse(string fileName)
-//        {
+        private Dictionary<string, string> LoadCustomers(string fileName)
+        {
+            Dictionary<string, string> customers = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(fileName);
 
-//        }
+            string line;
+            line = sr.ReadLine();
+            while (sr != null)
+            {
+                string[] parts = line.Split(',');
 
+                customers.Add(parts[0], parts[1]);
+            }
 
-//        #endregion
-//        #region CONSTRUCTORS
-//        public Supermarket(string name, string address, string fileCashiers, string fileCostumers, string fileItems, int activeLines)
-//        {
-//            this.name = name;
-//            this.address = address;
-//            activeLines = 1; // Default to 1 active line
-//            lines = new CheckOutLine[MAXLINES]; // Initialize checkout lines
-//        }
-//        #endregion
+            return customers;
+        }
 
+        private Dictionary<string, string> LoadCashiers(string fileName)
+        {
+            Dictionary<string, string> cashiers = new Dictionary<string, string>();
+            StreamReader sr = new StreamReader(fileName);
 
+            string line;
+            line = sr.ReadLine();
+            while (sr != null)
+            {
+                string[] parts = line.Split(',');
 
-//        Properties
-//        public string Name
-//        {
-//            get { return name; }
-//            set { name = value; }
-//        }
+                cashiers.Add(parts[0], parts[1]);
+            }
 
-//        public string Address
-//        {
-//            get { return address; }
-//            set { address = value; }
-//        }
+            return cashiers;
+        }
 
-//        public int ActiveLines
-//        {
-//            get { return activeLines; }
-//            set
-//            {
-//                if (value >= 1 && value <= MAXLINES)
-//                {
-//                    activeLines = value;
-//                }
-//                else
-//                {
-//                    throw new ArgumentException("Active lines must be between 1 and MAXLINES.");
-//                }
-//            }
-//        }
+        private Dictionary<string, double> LoadWarehouse(string fileName)
+        {
+            Dictionary<string, double> products = new Dictionary<string, double>();
+            StreamReader sr = new StreamReader(fileName);
 
-//        Method to initialize checkout lines
-//        public void InitializeCheckOutLines()
-//        {
-//            for (int i = 0; i < MAXLINES; i++)
-//            {
-//                lines[i] = new CheckOutLine(); // Initialize each checkout line
-//            }
-//        }
-//    }
+            string line;
+            line = sr.ReadLine();
+            while (sr != null)
+            {
+                string[] parts = line.Split(',');
+                products.Add(parts[0], Convert.ToDouble(parts[4]));
+            }
 
-//}
+            return products;
+        }
+    }
+}
