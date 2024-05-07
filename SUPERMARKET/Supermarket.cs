a870bc40e403b8df8709fcc45963c6cea0696b2e
Victor <a2308928@gmail.com>
2024-05-07 12:03:40 +0200

    j

--- SUPERMARKET/Supermarket.cs
+++ SUPERMARKET/Supermarket.cs
@@ -1,10 +1,10 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-/*namespace SUPERMARKET
+namespace SUPERMARKET
 {
     internal class Supermarket
     {
@@ -138,6 +138,6 @@
 
     }
 
-}*/
+}
 
     
