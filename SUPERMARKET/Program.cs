d960501cb43ad050cedd9f8129b1b5559780e355
Yary <a2308746@institutmontilivi.cat>
2024-05-13 19:44:14 +0200

    cambios main

--- SUPERMARKET/Program.cs
+++ SUPERMARKET/Program.cs
@@ -5,75 +5,45 @@
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            //TEST ITEM
 
-            Console.WriteLine("--------------------------------ITEMS------------------------------");
-            Item item1 = new Item(1, "Manzana",true, 1.50, Category.FRUITS, Packaging.Unit, 50, 10);
-            Item item2 = new Item(2, "Entrecot",false, 7.75, Category.MEAT, Packaging.Kg, 10, 20);
+            // CREAR CLIENTS
 
-            //TEST PERSON 
-            Console.WriteLine("--------------------------------PERSONS------------------------------");
-            Person cashier1 = new Cashier("91827364A", "Ferran Chic", new DateTime(2015, 4, 13));
-            Console.WriteLine(cashier1);
-
-            Person customer1 = new Customer("98765432B", "Victor Granados", 73692827);
-            Console.WriteLine(customer1);
-
-            //TEST CASHIER
-            Console.WriteLine("--------------------------------CASHIERS------------------------------");
-            Cashier cashier2 = new Cashier("12345678A", "Artur Juve", new DateTime(2014, 03, 24));
-            Console.WriteLine(cashier2);
-
-            cashier2.AddPoints(150);
-            Console.WriteLine($"+points: {cashier2}");
-
-            //TEST CUSTOMER 
-            Console.WriteLine("--------------------------------CUSTOMERS------------------------------");
-            Customer customer2 = new Customer("98765432B", "Yaryna Blanco", 9872432);
-            Console.WriteLine(customer2);
-
-            customer2.AddPoints(50);
-            Console.WriteLine($"+points: {customer2}");
-
-            customer2.AddInvoiceAmount(100.50);
-            Console.WriteLine($"+invoice: {customer2}");
-
-            //TEST SUPERMARKET 
-            Console.WriteLine("--------------------------------SUPERMARKET------------------------------");
+            Customer customer1 = new Customer("123456789A", "Yaryna Blanco", 1000);
+            customer1.AddInvoiceAmount(500); 
+            customer1.Active = true; 
+           
+            Customer customer2 = new Customer("987654321B", "Victor Granados",200);
+            customer2.AddInvoiceAmount(700); 
+            customer2.Active = false;
+                   
+            Console.WriteLine(customer1.ToString());         
+            Console.WriteLine(customer2.ToString());
 
 
-            Supermarket supermarket = new Supermarket("BonPreu", "Calle Montilivi");
+            //CREAR CAIXER
 
-            supermarket.LoadCustomers("CUSTOMERS.TXT");
-            supermarket.LoadCashiers("CASHIERS.TXT");
-            supermarket.LoadWarehouse("GROCERIES.TXT");
-
-            Console.WriteLine($"Nom: {supermarket.Name}");
-            Console.WriteLine($"Adreça: {supermarket.Address}");
-            Console.WriteLine($"Numero de linies obertes: {supermarket.ActiveLines}");
-
-            supermarket.ActiveLines = 3;
-            Console.WriteLine($"(+linies)Numero de linies obertes: {supermarket.ActiveLines}");
+            Cashier cashier = new Cashier("123456789C", "Ferran Chic", 500, new DateTime(2010, 3, 15));
 
 
 
-            //TEST ORDENACIO
-            Console.WriteLine("--------------------------------ITEMS ORDENATS------------------------------");
+            // Añadir una cantidad facturada
+            cashier.AddInvoiceAmount(1500);
+            cashier.AddInvoiceAmount(1500);
 
-            /*SortedSet<Item> itemsByStock = supermarket.GetItemByStock();
-            itemsByStock.Add(new Item(1, "Peix",true, 10.50, Category.FROZEN, Packaging.Unit, 20, 5));
-            itemsByStock.Add(new Item(2, "Platano",false,8.75, Category.FRUITS, Packaging.Unit, 15, 3));
-            itemsByStock.Add(new Item(3, "Pan",true, 5.25, Category.BREAD, Packaging.Unit, 25, 10));
-            itemsByStock.Add(new Item(4, "Pepino",false, 12.30, Category.VEGETABLES, Packaging.Unit, 18, 6));
-         
-            Console.WriteLine("Elementos ordenados por stock:");
-            foreach (Item item in itemsByStock)
-            {
-               Console.WriteLine(item.ToString());
-            }*/
+            // Mostrar información del cajero
 
+            Console.WriteLine(cashier.ToString());
 
+            #region ITEMS
+            Item item1 = new Item(1, "Manzanas", 1.5, Item.Category.FRUITS, Item.Packaging.Kg, 0, 0);
 
+            Console.WriteLine("Detalles del artículo antes de la oferta:");
+            Console.WriteLine(item1.ToString());
+
+            Console.ReadLine();
+
+           
+            #endregion
         }
     }
 }
